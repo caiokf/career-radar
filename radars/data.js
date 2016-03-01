919b0a64b1afbf15c808fc7c69018dd8463ea1b9
Caio Kinzel Filho <caiokf@gmail.com>
2016-03-01 11:28:55 +0100

    updates on radar items

--- radars/data.js
+++ radars/data.js
@@ -2,42 +2,41 @@ title = 'Caio Kinzel Filho, 2016';
 
 data = {
     'Languages': {
-        'C#': 4,
-        'Javascript' : 4,
-        'F#': 2
+      'C#': 4,
+      'Javascript' : 4,
+      'Typescript': 2,
+      'F#': 2,
+      'Haskell': 1
     },
 
     'Tools': {
-        'Eclipse':4,
-        'Git':3,
-        'EC2':2,
-        'Ivy':3,
-        'Ant':3,
-        'Vim':2,
-        'Svn':4,
-        'IntelliJ':1,
-        'Maven':1,
-        'Rake': 1,
+      'Atom':4,
+      'Git': 4,
+      'EC2': 2,
+      'Vim': 2,
+      'Sublime': 3,
+      'Svn': 1,
+      'Visual Studio': 3,
+      'Visual Studio Code': 2,
     },
 
     'Techniques': {
-        'TDD': 3,
-        'Design Patterns': 2,
-        'DDD': 2,
-        'BDD': 1,
-        'OOD': 4,
-        'DevOps':3,
-        'Refactoring':3,
+      'Continuous Delivery': 4,
+      'Microservice': 3,
+      'RESTful APIs': 3,
+      'TDD': 3,
+      'Design Patterns': 2,
+      'DDD': 4,
+      'OOD': 4,
+      'DevOps': 2,
+      'Refactoring': 3,
     },
 
     'Platforms': {
-        'Linux':4,
-        'Windows':3,
-    	'ESX/vSphere':3,
-        'Mac OSX': 1,
-        'Network stuffs': 3,
-        'Cisco': 2,
-        'Android': 2,
-        'iOS': 1,
+      'Linux': 2,
+      'Windows': 3,
+      'Mac OSX': 4,
+      'Android': 2,
+      'iOS': 1,
     }
 };
